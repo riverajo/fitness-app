9db9a57db9be52025b5d362ada4a324e92523ed5
riverajo <2325467+riverajo@users.noreply.github.com>
2025-11-21 08:34:08 -0500

    chore: Cleanup unused auto-generated code

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -268,18 +268,3 @@ func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *Resolver) WorkoutLog() WorkoutLogResolver { return &workoutLogResolver{r} }
-func (r *setInputResolver) Unit(ctx context.Context, obj *internalModel.SetInput, data model1.WeightUnit) error {
-	panic(fmt.Errorf("not implemented: Unit - unit"))
-}
-func (r *Resolver) SetInput() SetInputResolver { return &setInputResolver{r} }
-type setInputResolver struct{ *Resolver }
-*/
